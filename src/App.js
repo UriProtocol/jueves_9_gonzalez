9aaccb1125807e39e20511486d927b415649c823
UriProtocol <angel_3041210085@utd.edu.mx>
2023-02-10 14:27:15 -0600

    alexis

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router,Switch,Route,Link } from 'react-router-dom'
 import './App.css';
+// import img from './img/fotopro.jpeg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path='/' exact>
+          <div className='home-wrapper'>
+            <h1 className='home'>Home</h1>
+          </div>
+        </Route>
+        <Route path='/nombre'>
+          <div className='nombre-wrapper'>
+            <h1 className='nombre'>Uriel_González</h1>
+          </div>
+        </Route>
+        <Route path='/UTD'>
+          <div className='utd-wrapper'>
+            <h1 className='utd'>UTD</h1>
+          </div>
+        </Route>
+        <Route path='/carrera'>
+          <div className='carrera-wrapper'>
+            <h1 className='carrera'>Desarrollo de Software Multiplataforma</h1>
+          </div>
+        </Route>
+        <Route path='/logo'>
+          <div className='logo'></div>
+        </Route>
+        <Route path='/foto'>
+          <h2>Hola, busco empleo</h2>
+          {/* <img src={img}></img> */}
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
